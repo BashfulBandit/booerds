4219d9beda979df9a551c0ea5f7b8002d416497e
Davis Templeton <davistempleton3@gmail.com>
2018-07-12 19:46:11 -0400

    Cleaning up repo.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from localflavor.us.models import (
     USStateField,
     USZipCodeField,
@@ -7,38 +7,51 @@ from localflavor.us.models import (
 from django.urls import reverse
 
 # Create your models here.
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+class User(AbstractUser):
+    is_customer = models.BooleanField('customer status', default=False)
+    is_vendor = models.BooleanField('vendor statuc', default=False)
     street_address = models.CharField(max_length=100, blank=False)
     zipcode = USZipCodeField(blank=False)
     city = models.CharField(max_length=50, blank=False)
     state = USStateField(blank=False)
 
-    is_customer = models.BooleanField(default=True)
-    is_vendor = models.BooleanField(default=False)
-    # This is where the Customer and Vendor class change.
-    date_of_birth = models.DateField(blank=False)
-
-
-    def __str__(self):
-        return self.user.email
-
-    def get_absolute_url(self):
-        return reverse('bookstore:profile', kwargs={'id': self.user.id})
+class Cutomer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 class Vendor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
-    street_address = models.CharField(max_length=100, blank=False)
-    zipcode = USZipCodeField(blank=False)
-    city = models.CharField(max_length=50, blank=False)
-    state = USStateField(blank=False)
-
-    is_customer = models.BooleanField(default=False)
-    is_vendor = models.BooleanField(default=True)
-    # This is where the Customer and Vendor class change.
-
-    def __str__(self):
-        return self.user.email
-
-    def get_absolute_url(self):
-        return reverse('bookstore:profile', kwargs={'id': self.user.id})
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+# class Customer(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+#     street_address = models.CharField(max_length=100, blank=False)
+#     zipcode = USZipCodeField(blank=False)
+#     city = models.CharField(max_length=50, blank=False)
+#     state = USStateField(blank=False)
+#
+#     is_customer = models.BooleanField(default=True)
+#     is_vendor = models.BooleanField(default=False)
+#     # This is where the Customer and Vendor class change.
+#     date_of_birth = models.DateField(blank=False)
+#
+#
+#     def __str__(self):
+#         return self.user.email
+#
+#     def get_absolute_url(self):
+#         return reverse('bookstore:profile', kwargs={'id': self.user.id})
+#
+# class Vendor(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+#     street_address = models.CharField(max_length=100, blank=False)
+#     zipcode = USZipCodeField(blank=False)
+#     city = models.CharField(max_length=50, blank=False)
+#     state = USStateField(blank=False)
+#
+#     is_customer = models.BooleanField(default=False)
+#     is_vendor = models.BooleanField(default=True)
+#     # This is where the Customer and Vendor class change.
+#
+#     def __str__(self):
+#         return self.user.email
+#
+#     def get_absolute_url(self):
+#         return reverse('bookstore:profile', kwargs={'id': self.user.id})
